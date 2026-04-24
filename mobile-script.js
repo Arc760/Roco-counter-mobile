27ae03d48319eb63c28f5a70fc012289f8b82946
Arc760 <lxql1119@gmail.com>
2026-04-23 22:18:56 -0400

    Update mobile-script.js

--- mobile-script.js
+++ mobile-script.js
@@ -1,53 +1,53 @@
 // 数据
 const items = [
-  { name: "柴渣虫",type: ["火系"],count: 0,img: "./roco-image/chai.png"},
+  { name: "柴渣虫",type: ["火系"],count: 0,img: "roco-image/chai.png"},
 
-  { name: "双灯鱼", type: ["水系"],count: 0, img: "./roco-image/fish.png"},
+  { name: "双灯鱼", type: ["水系"],count: 0, img: "roco-image/fish.png"},
 
-  { name: "月牙雪熊", type: ["冰系"], count: 0, img: "./roco-image/bear.png"},
+  { name: "月牙雪熊", type: ["冰系"], count: 0, img: "roco-image/bear.png"},
 
-  { name: "粉粉星", type: ["电系"], count: 0, img: "./roco-image/star.png"},
+  { name: "粉粉星", type: ["电系"], count: 0, img: "roco-image/star.png"},
 
-  { name: "空空颅", type: "幽系", count: 0, img: "./roco-image/skull.png"},
+  { name: "空空颅", type: "幽系", count: 0, img: "roco-image/skull.png"},
 
-  { name: "嗜光嗡嗡", type: ["恶系"], count: 0, img: "./roco-image/mosquito.png"},
+  { name: "嗜光嗡嗡", type: ["恶系"], count: 0, img: "roco-image/mosquito.png"},
 
-  { name: "贝瑟", type: ["机械系"], count: 0, img: "./roco-image/pot.png"},
+  { name: "贝瑟", type: ["机械系"], count: 0, img: "roco-image/pot.png"},
 
-  { name: "粉星仔", type: "幻系", count: 0, img: "./roco-image/zai.png"},
+  { name: "粉星仔", type: "幻系", count: 0, img: "roco-image/zai.png"},
 
-  { name: "格兰种子", type: "草系", count: 0, img: "./roco-image/seed.png"},
+  { name: "格兰种子", type: "草系", count: 0, img: "roco-image/seed.png"},
 
-  { name: "奇丽草", type: "草系", count: 0, img: "./roco-image/grass.png"},
+  { name: "奇丽草", type: "草系", count: 0, img: "roco-image/grass.png"},
 
-  { name: "治愈兔", type: ["火系"], count: 0, img: "./roco-image/rabbit.png"},
+  { name: "治愈兔", type: ["火系"], count: 0, img: "roco-image/rabbit.png"},
 
-  { name: "呼呼猪", type: ["冰系"], count: 0, img: "./roco-image/pig.png"},
+  { name: "呼呼猪", type: ["冰系"], count: 0, img: "roco-image/pig.png"},
 
-  { name: "大耳帽兜", type: ["冰系"], count: 0, img: "./roco-image/dou.png"},
+  { name: "大耳帽兜", type: ["冰系"], count: 0, img: "roco-image/dou.png"},
 
-  { name: "拉特", type: "电系", count: 0, img: "./roco-image/rai.png"},
+  { name: "拉特", type: "电系", count: 0, img: "roco-image/rai.png"},
 
-  { name: "恶魔狼", type: "恶系", count: 0, img: "./roco-image/wolf.png"},
+  { name: "恶魔狼", type: "恶系", count: 0, img: "roco-image/wolf.png"},
 
-  { name: "机械方方", type: "机械系", count: 0, img: "./roco-image/cube.png"},
+  { name: "机械方方", type: "机械系", count: 0, img: "roco-image/cube.png"},
 
-  { name: "绒绒", type: "绒绒", count: 0, img: "./roco-image/rong.png"},
+  { name: "绒绒", type: "绒绒", count: 0, img: "roco-image/rong.png"},
 
-  { name: "犀角鸟", type: "犀角鸟", count: 0, img: "./roco-image/mop.png"},
+  { name: "犀角鸟", type: "犀角鸟", count: 0, img: "roco-image/mop.png"},
 
-  { name: "火红尾", type: "火系", count: 0, img: "./roco-image/horse.png"},
+  { name: "火红尾", type: "火系", count: 0, img: "roco-image/horse.png"},
 
-  { name: "果冻", type: "水系", count: 0, img: "./roco-image/jelly.png"},
+  { name: "果冻", type: "水系", count: 0, img: "roco-image/jelly.png"},
 
-  { name: "星尘虫", type: "虫系", count: 0, img: "./roco-image/ladybug.png"},
+  { name: "星尘虫", type: "虫系", count: 0, img: "roco-image/ladybug.png"},
 
-  { name: "影狸", type: "幽系", count: 0, img: "./roco-image/fox.png"},
+  { name: "影狸", type: "幽系", count: 0, img: "roco-image/fox.png"},
 ];
 
 // 渲染
 function render() {
-  console.log("render running");
+  console.log("render running", container);
   const container = document.getElementById("container");
   container.innerHTML = "";
 
